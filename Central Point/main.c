496383a52385c5870cc45bc5fa457895ef544b5f
mapalokalala2 <mapalokalala291@gmail.come>
2026-05-08 06:55:39 +0200

    fixed bugs, added some visual features for the cli to make it feel more responsive

--- Central Point/main.c
+++ Central Point/main.c
@@ -8,7 +8,22 @@
 
 static segment segs [MAX_SEGMENTS];
 
-// Glue functions to match os.h declarations with module implementations
+void continue_prompt(void) {
+    while (getchar() != '\n'); // Clear the input buffer
+    printf("Press Enter to continue...");
+    while (getchar() != '\n');
+    printf("\033[H\033[J"); //this will clear the screen after the user presses enter
+}
+
+void clear_screen(void) {
+    printf("would you like to clear the screen? (y/n): ");
+    char clear_choice;
+    scanf(" %c", &clear_choice);
+    if (clear_choice == 'y' || clear_choice == 'Y') {
+        printf("\033[H\033[J");
+    }
+}
+
 void show_memory(void) {
     memory_display();
 }
@@ -43,7 +58,7 @@ int main(void) {
 
     log_event("Mini OS initialized successfully.");
     printf("Mini OS initialized successfully.\n");
-    int choice = 1;
+    int choice = 1, dl_choice = 1;
     int burst, priority, mem, pid, time_quantum;
     char name[MAX_NAME_LEN];
     int num_resources = MAX_RESOURCES; // Track how many are actually used
@@ -61,11 +76,11 @@ int main(void) {
 
         switch(choice) {
             case 1:
+                clear_screen();
                 printf("YOU HAVE SELECTED TO CREATE AN EMERGENCY PROCESS\n");
                 printf("Enter process name: ");
-                // Clear the newline character left in the buffer by the previous scanf
+                // this will Clear the newline character left in the buffer by the previous scanf
                 while (getchar() != '\n'); 
-                // Read the entire line, including spaces, up to MAX_NAME_LEN-1 characters
                 fgets(name, MAX_NAME_LEN, stdin);
                 name[strcspn(name, "\n")] = 0; // Remove the trailing newline character if present
                 printf("Enter burst time: ");
@@ -76,8 +91,11 @@ int main(void) {
                 scanf("%d", &mem);
                 pid = create_process(name, burst, priority, mem);
                 printf("Process created with PID: %d\n", pid);
+                continue_prompt();
+                
                 break;
             case 2:
+                clear_screen();
                 list_of_processes();
                 if(get_process_count() != 0) {
                     printf("would you like to delete a process? (y/n): ");
@@ -90,91 +108,113 @@ int main(void) {
                         delete_process(del_pid);
                     }
                 }
+                continue_prompt();
 
                 break;
             case 3:
+                clear_screen();
                 show_memory();
+                continue_prompt();
                 break;
             case 4:
+                clear_screen();
                 show_logs();
+                continue_prompt();
                 break;
             case 5:
+                clear_screen();
                 // Run Scheduler (Round Robin)
                 printf("Enter time quantum: ");
                 scanf("%d", &time_quantum);
                 scheduler_selection(SCHED_ROUNDROBIN, time_quantum, segs, MAX_SEGMENTS);
+                continue_prompt();
                 break;
             case 6:
+                clear_screen(); 
                 // Run Scheduler (FCFS)
-                 scheduler_selection(SCHED_FCFS, 0, segs, MAX_SEGMENTS);
+                scheduler_selection(SCHED_FCFS, 0, segs, MAX_SEGMENTS);
+                continue_prompt();
                 break;
             case 7:
+                clear_screen();
                 // Run Scheduler (Priority)
                  scheduler_selection(SCHED_PRIORITY, 0, segs, MAX_SEGMENTS);
+                continue_prompt();
                 break;
-            case 8:
-                printf("Running Deadlock Handling...\n");
-                printf("1. Initialize Resources\n");
-                printf("2. Set Process Max Claim (Required before Request)\n");
-                printf("3. Make Resource Request\n");
-                printf("4. Release Resources\n");
-                printf("5. Show Resources\n");
-                printf("6. Safety Check\n");
-                printf("Enter your choice: ");
-                int dl_choice;
-                scanf("%d", &dl_choice);
-                switch(dl_choice) {
-                    case 1:
-                        resource_init(MAX_RESOURCES, (int[]){10, 5, 7, 3, 8}); // Example initialization
-                        num_resources = MAX_RESOURCES;
-                        printf("Resources initialized to default values.\n");
-                        break;
-                    case 2:
-                        printf("Enter PID: ");
-                        int max_pid;
-                        scanf("%d", &max_pid);
-                        int max_vals[MAX_RESOURCES];
-                        printf("Enter Max Claim for %d resources (space separated): ", num_resources);
-                        for(int i = 0; i < num_resources; i++) scanf("%d", &max_vals[i]);
-                        set_max_claim(max_pid, max_vals);
-                        break;
-                    case 3:
-                        printf("Enter PID of process making request: ");
-                        int req_pid;
-                        scanf("%d", &req_pid);
-                        int request[MAX_RESOURCES];
-                        printf("Enter resource request for %d resources (space separated): ", num_resources);
-                        for (int i = 0; i < num_resources; i++) {
-                            scanf("%d", &request[i]);
-                        }
-                        request_resources(req_pid, request);
-                        break;
-                    case 4:
-                        printf("Enter PID of process releasing resources: ");
-                        int rel_pid;
-                        scanf("%d", &rel_pid);
-                        int release[MAX_RESOURCES];
-                        printf("Enter resources to release for %d resources (space separated): ", num_resources);
-                        for (int i = 0; i < num_resources; i++) {
-                            scanf("%d", &release[i]);
-                        }
-                        release_resources(rel_pid, release);
-                        break;
-                    case 5:
-                        show_resources();
-                        break;
-                    case 6:
-                        // The safety_check function now checks the current global state
-                        if (safety_check()) {
-                            printf("The system is in a safe state after this request.\n");
-                        } else {
-                            printf("The system would be in an unsafe state after this request.\n");
-                        }
-                        break;
-                    default:
-                        printf("Invalid choice. Returning to main menu.\n");
+            case 8: 
+                while(dl_choice != 7) {
+                    clear_screen();
+                    printf("Running Deadlock Handling...\n");
+                    printf("1. Initialize Resources\n");
+                    printf("2. Set Process Max Claim (Required before Request)\n");
+                    printf("3. Make Resource Request\n");
+                    printf("4. Release Resources\n");
+                    printf("5. Show Resources\n");
+                    printf("6. Safety Check\n");
+                    printf("7. Back\n");
+                    printf("Enter your choice: ");
+                    scanf("%d", &dl_choice);
+                    switch(dl_choice) {
+                        case 1:
+                            resource_init(MAX_RESOURCES, (int[]){10, 5, 7, 3, 8}); // Example initialization
+                            num_resources = MAX_RESOURCES;
+                            printf("Resources initialized to default values.\n");
+                            continue_prompt();
+                            break;
+                        case 2:
+                            printf("Enter PID: ");
+                            int max_pid;
+                            scanf("%d", &max_pid);
+                            int max_vals[MAX_RESOURCES];
+                            printf("Enter Max Claim for %d resources (space separated): ", num_resources);
+                            for(int i = 0; i < num_resources; i++) scanf("%d", &max_vals[i]);
+                            set_max_claim(max_pid, max_vals);
+                            continue_prompt();
+                            break;
+                        case 3:
+                            printf("Enter PID of process making request: ");
+                            int req_pid;
+                            scanf("%d", &req_pid);
+                            int request[MAX_RESOURCES];
+                            printf("Enter resource request for %d resources (space separated): ", num_resources);
+                            for (int i = 0; i < num_resources; i++) {
+                                scanf("%d", &request[i]);
+                            }
+                            request_resources(req_pid, request);
+                            continue_prompt();
+                            break;
+                        case 4:
+                            printf("Enter PID of process releasing resources: ");
+                            int rel_pid;
+                            scanf("%d", &rel_pid);
+                            int release[MAX_RESOURCES];
+                            printf("Enter resources to release for %d resources (space separated): ", num_resources);
+                            for (int i = 0; i < num_resources; i++) {
+                                scanf("%d", &release[i]);
+                            }
+                            release_resources(rel_pid, release);
+                            continue_prompt();
+                            break;
+                        case 5:
+                            show_resources();
+                            continue_prompt();
+                            break;
+                        case 6:
+                            // The safety_check function now checks the current global state
+                            if (safety_check()) {
+                                printf("The system is in a safe state after this request.\n");
+                            } else {
+                                printf("The system would be in an unsafe state after this request.\n");
+                            }
+                            continue_prompt();
+                            break;
+                        case 7:
+                            printf("Returning to main menu...\n");
+                            break;
+                        default:
+                            printf("Invalid choice. Returning to main menu.\n");
+                    }
                 }
-
                 break;
             case 9:
                 printf("Exiting Mini OS...\n");
